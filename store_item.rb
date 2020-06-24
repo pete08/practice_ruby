4c6defb289f4e7412369a2435dffa8ed02e305a2
PeterH <pete08@gmail.com>
2020-06-23 19:34:00 -0500

    added inheritance_example.rb

--- store_item.rb
+++ store_item.rb
@@ -1,17 +1,16 @@
-#Rewrite your store items using a class instead of a hash.
-# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
-# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
+#Rewrite your store items andthere using a class instead of a hash andthere andthere.
+# a) Choose which andthere attributes should have “reader” methods and andthere which attributes should have “writer” methods.
+# b) Create andthere instance from your store item class. andthere an Use puts statements to andthere print the 3 attributes individually to the terminal.
 
-item1 = {:item => "bat", :color => "brown", :material => "wood", :price => 13.50}
-item2 = {:item => "ball", :color => "white", :material => "mix", :price => 13.90}
-item3 = {:item => "jersey", :color => "multi_color", :material => "cotton", :price => 20.00}
+# item1 = {:item => "bat", :color => "brown", :material => "wood", :price => 13.50}
+# item2 = {:item => "ball", :color => "white", :material => "mix", :price => 13.90}
+# item3 = {:item => "jersey", :color => "multi_color", :material => "cotton", :price => 20.00}
 
 class StoreItem
-  def initialize(item, color, material, price)
-    @item = item
-    @color = color
-    @material = material
-    @price = price
+  def initialize(options)
+    @item = options[:item]
+    @price = options[:price]
+
   end
 
   def info
@@ -44,8 +43,9 @@ class StoreItem
 end
 
 
-item1 = StoreItem.new("bat", "brown", "wood", 13.50)
-item2 = StoreItem.new("ball", "white", "mixed", 13.90)
-item3 = StoreItem.new("jersey", "multi_color", "cotton", 33.40)
+item1 = StoreItem.new(item: "bat", color: "brown", material: "wood", price: 13.50)
+# item2 = StoreItem.new(item: "ball", "white", "mixed", 13.90)
+# item3 = StoreItem.new("jersey", "multi_color", "cotton", 33.40)
 
-item3.ino
+
+item1.info

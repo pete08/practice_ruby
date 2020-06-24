5b7d59d1e8849dc615c60bfa5c160a22c26b11c0
PeterH <pete08@gmail.com>
2020-06-23 20:44:10 -0500

    add inheretence class to store-item and manager.rb

--- store_item.rb
+++ store_item.rb
@@ -6,29 +6,38 @@
 # item2 = {:item => "ball", :color => "white", :material => "mix", :price => 13.90}
 # item3 = {:item => "jersey", :color => "multi_color", :material => "cotton", :price => 20.00}
 
+
+
 class StoreItem
   def initialize(options)
     @item = options[:item]
+    @brand = options[:brand]
+    @taste = options[:taste]
     @price = options[:price]
+  end
 
+end
+
+class Food < StoreItem
+  def initialize(options)
+    super
+    @shelf_life = options[:shelf_life]
   end
 
   def info
-    p "item: #{@item}"
-    p "price: #{@price}"
+    p "this #{@item}: is #{@brand}, #{@taste}. and costs #{@price}. shelf life is #{@shelf_life}."
   end
-
 
   def item
     @item
   end
   
-  def color
-    @color
+  def brand
+    @brand
   end
   
-  def material
-    @material
+  def taste
+    @taste
   end
   
   def price
@@ -39,13 +48,24 @@ class StoreItem
     @price = new_price
   end
 
+end
+
+class Bread < Food
+  def sliced
+    @sliced = false
+  end
+
+  def sliced=(x)
+    @sliced = x
+  end
 
 end
 
 
-item1 = StoreItem.new(item: "bat", color: "brown", material: "wood", price: 13.50)
+
+bread1 = Bread.new(item: "bread", name: "brownberry", taste: "grainy", price: 3.50, shelf_life: "4 days")
 # item2 = StoreItem.new(item: "ball", "white", "mixed", 13.90)
 # item3 = StoreItem.new("jersey", "multi_color", "cotton", 33.40)
 
 
-item1.info
+bread1.info

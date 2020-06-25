a822347a8a04a2b49b2f02b3f40ae0c39e050791
PeterH <pete08@gmail.com>
2020-06-24 21:32:35 -0500

    add scratch1 file to list favorite foods

--- scratch1.rb
+++ scratch1.rb
@@ -1,12 +1,13 @@
 
-food_list = []
-5.times do
-  p "what's a food?"
-  food = gets.chomp
-  food_list << food
+food_list = ["pizza", "icecream", "jam", "butter", "bread"]
+# 5.times do
+#   p "what's a food?"
+#   food = gets.chomp
+#   food_list << food
+# end
+
+x = 1
+food_list.each do |n|
+  puts "#{x}. #{n}"
+  x += 1
 end
-
-p food_list
-
-
-
